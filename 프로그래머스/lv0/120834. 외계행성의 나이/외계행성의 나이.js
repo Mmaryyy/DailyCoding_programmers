0539058f642a85ced3ae5a3256d4ae22085b9d13
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-05 09:38:13 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,12 @@
+function solution(age) {
+    //변환할 알파벳은 그 알파벳 요소의 인덱스임
+    const alpabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let ageStr = '' + age;
+    let result = ''
+    
+    for(let i = 0; i < ageStr.length; i++) {
+        result += alpabet[ageStr[i]]
+        }
+    
+    return result
+}
