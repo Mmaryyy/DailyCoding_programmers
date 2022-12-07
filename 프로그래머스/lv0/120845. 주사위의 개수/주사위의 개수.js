91486b586871cd74d6c0baa5ff985b20edbe137a
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-07 09:44:32 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,8 @@
+function solution(box, n) {
+    // box = [상자의 가로, 상자의 세로, 상자의 높이]
+    // n = 주사위 모서리의 길이
+    // 주사위가 최대로 들어가려면
+    // 각 변을 주사위 모서리로 나눈 것의 몫을 다 곱하면 됨.
+    
+    return box.map(ele => parseInt(ele / n)).reduce((acc, cur) => acc * cur, 1)
+}
