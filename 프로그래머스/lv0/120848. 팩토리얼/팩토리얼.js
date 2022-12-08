07df70b968f73334c904a3fa8ba58cad88f4dccc
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-08 10:13:09 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,12 +1,4 @@
 function solution(n) {
-    let result = 1
-    let i = 1
-    
-    while (result < n) {
-        i++
-        result *= i
-    } 
-    
-    if (result > n) return i - 1
-    return i
+    var answer = 0;
+    return answer;
 }
