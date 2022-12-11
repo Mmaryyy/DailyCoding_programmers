51207b8742769c95beb25b21ee39ef404f4db2f8
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-11 19:53:12 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,13 @@
+function solution(babbling) {
+    // 배열을 순회하며 각 요소(스트링 형태)를 조카가 말할 수 있는 어휘 단위로 split
+    // split 한다는 것 -> 이것을 쪼개는 것 -> 말할 수 없는 단어만 남음
+    // 말할 수 있는 단어가 4개 -> 정규 표현식으로 묶어 표현
+    // 원래 babbling의 길이에서 말할 수 없는 단어만 남은 배열의 길이를 뺀 값
+    
+    let speakableWord = []
+    
+    for (let el of babbling) {
+        speakableWord.push(el.split(/aya|ye|woo|ma/).join(''))
+    }
+    return babbling.length - speakableWord.filter(ele => ele.length !== 0).length
+  }
