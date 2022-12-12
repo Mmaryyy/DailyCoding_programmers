b5559d709c804aeed132ce976734b401837262ed
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-12 18:23:03 +0900

    [level 0] Title: 가까운 수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,22 @@
+function solution(array, n) {
+    // array 순회하면서 요소 - n 을 빼고
+    // 계산값이 음수일 때 양수로 바꾸기
+    // 가장 작은 차이값과 그 요소를 저장해두고
+    // 저장해 둔 값보다 적은 차이값이 나타나면 교체
+    
+    let min = Math.abs(array[0] - n)
+    let gap = 0
+    let closerNum = array[0]
+    
+    for (let i = 1; i < array.length; i++) {
+        gap = Math.abs(array[i] - n)
+        if (gap < min) {
+            min = gap
+            closerNum = array[i]
+        } else if (gap === min) {
+            closerNum = closerNum < array[i] ? closerNum : array[i]
+        }
+        continue
+    }
+    return closerNum
+}
