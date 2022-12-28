21eb17f8fdf650b9682c49196744a52fa982591c
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-28 16:46:10 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,12 @@
+function solution(numlist, n) {
+    // 절대값으로 비교하고
+    // 분기 -> 절대값이 큰 걸 뒤로 보내기
+    // 분기2 -> 절대값이 같으면 두 숫자의 크고 작음을 비교해서 순서 세우기
+    
+        const compare = (num1, num2) => {
+        if (Math.abs(num1-n) === Math.abs(num2-n)) return num2 - num1
+        return Math.abs(num1-n) - Math.abs(num2-n)
+    }
+
+    return numlist.sort(compare)
+}
