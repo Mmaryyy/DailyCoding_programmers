54c28544ec15a40bc0bc0bf3c200f483c3138de1
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2022-12-29 16:51:47 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,19 +1,13 @@
 function solution(n) {
     const noThree = []
-  
-    // for (let i = 1; i < 3*n; i++) {
-    //     if (i % 3 !== 0) {
-    //       let char = String(i)
-    //       if (!char.includes('3')) noThree.push(i)
-    //   }
-    // }
     let num = 1
+    
     while (noThree.length < n) {
         if (num % 3 !== 0) {
-            let char = String(num)
-            if (!char.includes('3')) noThree.push(num)
+            if (!String(num).includes('3')) noThree.push(num)
         }
         num++
     }
+    
     return noThree[n-1]
   }
