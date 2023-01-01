398eaecf369b28f9a5d2336472a0915255a6ffcb
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-01-01 15:16:22 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -3,22 +3,11 @@ function solution(score) {
     // 각 배열의 평균 점수로 이루어진 배열을 다시 정렬해서 등수(인덱스..가 될 수 없네)로 정렬된 새로운 배열
     // 값이 같으면 같은 값을 내보내야 함..
 
-    const getAve = ([ english, math ]) => {
+    const aveArr = score.map(([ english, math ]) => {
         return (english + math) / 2
-    }
-
-    let aveArr = []
-
-    for (let student of score) {
-        aveArr.push(getAve(student))
-    }
-
-    let sortedArr = [...aveArr].sort((a,b) => b-a)
-    let result = []
-
-    for (let aveScore of aveArr) {
-        result.push(sortedArr.indexOf(aveScore) + 1)
-    }
+    })
     
-    return result
+    let sortedArr = [...aveArr].sort((a,b) => b-a)
+    
+    return aveArr.map(el => sortedArr.indexOf(el) + 1)
 }
