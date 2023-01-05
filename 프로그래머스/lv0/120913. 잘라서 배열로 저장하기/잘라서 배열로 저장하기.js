9ccc26d88a9ec70d828598f68e8ca49871f5d5a7
Mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-01-06 00:53:28 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,12 @@
+function solution(my_str, n) {
+    // my_str를 쪼갠 요소 변수 하나 설정하고
+    // 길이가 n이 될 때까지 담음 -> 이걸 answer 배열에 담음 slice
+    // length 넘어가면 길이까지만 자름
+
+    const result = []
+    for (let i = 0; i * n < my_str.length; i++) {
+        result.push(my_str.slice(i*n, (i+1)*n))
+    }
+
+    return result
+}
