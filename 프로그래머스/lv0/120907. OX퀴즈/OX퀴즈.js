5b8b08fac4dfb64c4274d6b3c74e2ab93aeee867
이혜림 <115970101+Mmaryyy@users.noreply.github.com>
2023-01-25 08:52:44 +0900

    [level 0] Title: OX퀴즈, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,19 @@
+function solution(quiz) {
+    // x[연산자]y = z 형태의 문자열 배열 quiz를 입력받아
+    // 수식의 옳고 그름에 따라 o, x 로 담긴 배열 출력
+    // quiz를 map 한다
+    // 
+
+    return quiz.map((el) => {
+        // el은 문자열 전체
+        // 각 수식으로 재가공
+        let [x, operator, y, equal, result] = el.split(' ')
+        if (operator === '+') {
+            if (parseInt(x) + parseInt(y) === parseInt(result)) return 'O'
+            return 'X'
+        } else {
+            if (parseInt(x) - parseInt(y) === parseInt(result)) return 'O'
+            return 'X'
+        }
+    })
+}
