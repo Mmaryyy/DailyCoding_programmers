32d7677337d979494f2f6d717b15497ba18bc190
이혜림 <115970101+Mmaryyy@users.noreply.github.com>
2023-01-31 14:16:50 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,3 @@
 function solution(s1, s2) {
-    let count = 0
-    for (let firstEle of s1) {
-        if (s2.indexOf(firstEle) !== -1) {
-            count++
-        }
-    }
-    return count
+    return s1.filter(el => s2.includes(el)).length
 }
