8a6701ecbf5b20ab0a84067f3953fbeb8412f301
이혜림 <115970101+Mmaryyy@users.noreply.github.com>
2023-02-06 17:59:38 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,4 +1,4 @@
 function solution(order) {
     const clap = ['3', '6', '9']
-    return String(order).split('').filter(el => clap.includes(el)).length
+    return [...String(order)].filter(el => clap.includes(el)).length
 }
