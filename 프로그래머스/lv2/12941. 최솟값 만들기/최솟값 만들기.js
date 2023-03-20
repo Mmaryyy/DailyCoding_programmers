39c22aaa3005fca073499072291613aae50dd069
mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-03-20 16:17:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.76 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,14 +2,8 @@ function solution(A,B){
     // 곱셈의 합이 최소값이 되려면
     // 오름차순 - 내림차순으로 각각 정렬된 요소의 곱을 더하면 된다
     
-    const sortedA = A.sort((a, b) => b - a)
-    const sortedB = B.sort((a, b) => a - b)
+    A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
     
-    const result = []
-    
-    for (let i = 0; i < sortedA.length; i++) {
-        result.push(sortedA[i] * sortedB[i])
-    }
-    
-    return result.reduce((acc, cur) => acc + cur, 0)
+    return A.reduce((acc, cur, idx) => acc + cur * B[idx], 0)
 } 
