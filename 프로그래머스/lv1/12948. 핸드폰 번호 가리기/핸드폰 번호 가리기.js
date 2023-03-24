84d38bb219a9f2b9cef69782fc64bb5db4953d3e
mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-03-24 11:58:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,3 @@
 function solution(phone_number) {
-    let maskingNum = phone_number.slice(0, -4).replaceAll(/[0-9]/g, '*')
-    return maskingNum + phone_number.slice(-4)
+    return '*'.repeat(phone_number.length - 4) + phone_number.slice(-4)
 }
