a4e199d2de8b3542e3d507578c96f1f56771a011
mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-05-22 12:33:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,4 +1,7 @@
 function solution(num) {
+  // 예외처리 2. 카운트가 500번을 넘어가면 -1을 반환하고 종료
+  // num이 짝수면 2로 나누고
+  // num이 홀수면 4을 곱하고 1을 더한다
   
   let count = 0
   let number = num
@@ -6,15 +9,11 @@ function solution(num) {
   // 예외처리 1. num === 1 return 0
   if (num === 1) return 0
 
-  while (count < 500) {
-
+  while (number !== 1 && count < 500) {
   count ++
 
-    // num이 짝수면 2로 나누고
     if (number % 2 === 0) {
       number = number / 2
-        
-    // num이 홀수면 4을 곱하고 1을 더한다
     } else {
       number = number * 3 + 1
     }
@@ -24,6 +23,5 @@ function solution(num) {
     }
   }
 
-  // 예외처리 2. 카운트가 500번을 넘어가면 -1을 반환하고 종료
   return count >= 500 ? -1 : count
 }
