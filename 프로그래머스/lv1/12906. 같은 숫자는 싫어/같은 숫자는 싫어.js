905635f0c1a331abf40fd6bf3de3f8cf4d1f65b2
mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-05-23 15:48:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.85 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let result = [arr[0]]
+    let pre = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (pre === arr[i]) continue
+        result.push(arr[i])
+        pre = arr[i]
+    }
+    return result
+}
