14bf6acca4cb5b3ea9330def6afa1317dd434c66
mmaryyy <115970101+Mmaryyy@users.noreply.github.com>
2023-07-11 17:45:24 +0900

    [level 1] Title: 시저 암호, Time: 7.93 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,20 @@
+function solution(s, n) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  let result = ''
+  for (let i = 0; i < s.length; i++) {
+      // 해당 문자가 대문자인지 여부를 저장해둠
+      if (s[i] === ' ') { 
+          result += ' '
+          continue
+      }
+      let isUpper = s[i] === s[i].toUpperCase()
+      let originalIdx = isUpper ? alphabet.indexOf(s[i]) : alphabet.indexOf(s[i].toUpperCase())
+      let nextIdx = originalIdx + n > 25 ? originalIdx + n - 25 - 1 : originalIdx + n
+      if (isUpper) {
+          result += alphabet[nextIdx]
+      } else {
+          result += alphabet[nextIdx].toLowerCase()
+      }    
+  }
+  return result
+}
